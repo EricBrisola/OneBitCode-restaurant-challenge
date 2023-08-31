ef7aa79e3d912ba839f858ff9651c4e960f2f43e
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-08-31 13:04:54 -0300

    Kitchen - Delete Button
    Added a delete button in kitchen page and added style for it

--- src/kitchen.js
+++ src/kitchen.js
@@ -16,10 +16,12 @@ const renderOrders = () =>
     {
       const order = document.createElement('li')
       const delButton = document.createElement('button')
-      order.textContent = 'Table: '+ el.table  + ' | item: ' + el.item + ' | quant: ' + el.quantity
+      order.textContent = 'Table: '+ el.table  + ' | item: ' + el.item + ' | Quantity: ' + el.quantity
       order.setAttribute('class', 'order-items')
       orders.appendChild(order)
-      delButton.setAttribute('textContent', 'delete') 
+      //delButton.setAttribute('textContent', 'delete') 
+      delButton.textContent = 'Delete'
+      delButton.setAttribute('class', 'delBtnStyle')
       order.appendChild(delButton)
     })
   }
