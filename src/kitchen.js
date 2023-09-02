a3560fef019be9ad0966ddb90a53f84520875bc3
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-09-02 17:21:21 -0300

    Kitchen Updates
    I worked in the kitchen page, added a check button to crossout the items that have been finished.

--- src/kitchen.js
+++ src/kitchen.js
@@ -16,7 +16,11 @@ const renderOrders = () =>
     allOrders.forEach((el) => 
     {
       const order = document.createElement('li')
-      order.textContent = 'n.º '+ el.table + ' | item: ' + el.item + ' | Quantity: ' + el.quantity
+      const paragraph = document.createElement('p')
+
+      paragraph.textContent = 'n.º '+ el.table + ' | item: ' + el.item + ' | Quantity: ' + el.quantity
+
+      order.appendChild(paragraph)
       order.setAttribute('class', 'order-items')
       order.setAttribute('data', i)
       orders.appendChild(order) 
@@ -25,16 +29,29 @@ const renderOrders = () =>
       delButton.textContent = 'X'
       delButton.setAttribute('class', 'delBtnStyle')
       delButton.addEventListener('click', deleteItemBtn)
-      order.appendChild(delButton)
+
+      const doneButton = document.createElement('button')
+      doneButton.textContent = '✓'
+      doneButton.setAttribute('class', 'doneBtnStyle')
+      doneButton.addEventListener('click', crossOutItem)
+
+      const spanButtons = document.createElement('span')
+      spanButtons.setAttribute('class', 'spanButtons')
+      spanButtons.appendChild(delButton)
+      spanButtons.appendChild(doneButton)
+
+      //order.appendChild(delButton)
+      order.appendChild(spanButtons)
       i++
     })
   }
 }
 
+// function to delete an order from the list
 function deleteItemBtn (btn)
 {
-  const itemToDelete = btn.currentTarget.parentNode
-  
+  const itemToDelete = btn.currentTarget.parentNode.parentNode
+
   let index = itemToDelete.getAttribute('data')
   itemToDelete.remove()
   allOrders.splice(index, 1)
@@ -42,11 +59,18 @@ function deleteItemBtn (btn)
   updateIndexes()
 }
 
+// function to cross out an order from the list
+function crossOutItem (btn){
+  const itemToCrossOut = btn.currentTarget.parentNode.parentNode.firstChild
+  itemToCrossOut.classList.toggle("crossedOut")
+}
+
+// function to update the orders in the local storage
 function updateOrders () {
   localStorage.setItem('orders', JSON.stringify(allOrders))
 }
 
-
+// function to update the indexes of the orders after deleting one
 function updateIndexes () {
   let newIndex = 0
   const allOrdersList = document.querySelectorAll('li')
