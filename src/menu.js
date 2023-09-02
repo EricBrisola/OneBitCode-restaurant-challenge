aeda71cf9b5d5a06e32347f9baafada53de1c9a3
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-09-02 15:03:01 -0300

    Items description + table number validation
    Added a validation in the table number and added a description on cards. I also updated the JS for Kitchen area to pickup the correct children

--- src/menu.js
+++ src/menu.js
@@ -1,50 +1,59 @@
 // Global variables //
 const parentCards = document.querySelectorAll('.food-cards')
 const orderList = []
-// Functions //
 
 
-// Order Button //
+// **Order Button 
 // **In event listner the anonymous function pick up all items that the customer wants in your Order - Return H4 and Input Number value
 
 const orderBtn = document.getElementById('order-btn')
 
 orderBtn.addEventListener('click', function (){
     const tableNumber = document.getElementById('table-number').value
-    parentCards.forEach((currentElement) => {
-        let numberInputs = currentElement.querySelectorAll("input[type='number']")
-    
-        numberInputs.forEach((input) => {
-            if (input.value !== '' && input.value !== undefined && input.value > 0) 
-            {
-                //console.log(input.parentNode.children[1])
-                let item = input.parentNode.children[1].textContent
-                let quantity = input.parentNode.children[2].value
-                const order = {item: item, quantity: quantity, table: tableNumber}
-                //console.log(order)
-                if(localStorage.length < 1)
-                {
-                    orderList.push(order)
-                    localStorage.setItem("orders", JSON.stringify(orderList))
-                    console.log(`local storage vazio, criei agr`)
-                }
-                else
-                {
-                    let allOrders = JSON.parse(localStorage.getItem('orders')) || []
-                    allOrders.push(order)
-                    localStorage.setItem('orders', JSON.stringify(allOrders))
-                    console.log(`local storage ja possui itens, soh adicionei mais`)
-                }
-                
-            }
-        })
-    })
-    alert('Order Finished !')
-    erasingInputFilds()
+    if (tableNumber == '' || tableNumber == undefined || tableNumber == null){
+        alert('Please enter a valid table number')
+        return
+    }
 
-    console.log(orderList)
+    else{
+        parentCards.forEach((currentElement) => {
+            let numberInputs = currentElement.querySelectorAll("input[type='number']")
+        
+            numberInputs.forEach((input) => {
+                if (input.value !== '' && input.value !== undefined && input.value > 0) 
+                {
+                    //console.log(input.parentNode.children[1])
+                    let item = input.parentNode.children[1].textContent
+                    let quantity = input.parentNode.children[3].value
+                    const order = {item: item, quantity: quantity, table: tableNumber}
+                    //console.log(order)
+                    if(localStorage.length < 1)
+                    {
+                        orderList.push(order)
+                        localStorage.setItem("orders", JSON.stringify(orderList))
+                        console.log(`local storage vazio, criei agr`)
+                    }
+                    else
+                    {
+                        let allOrders = JSON.parse(localStorage.getItem('orders')) || []
+                        allOrders.push(order)
+                        localStorage.setItem('orders', JSON.stringify(allOrders))
+                        console.log(`local storage ja possui itens, soh adicionei mais`)
+                    }
+                    
+                }
+            })
+        })
+        alert('Order Finished !')
+        erasingInputFilds()
+    
+        console.log(orderList)
+    }
+
 })
 
+
+// **Function to clear the input fields after the order is finished
 function erasingInputFilds () 
 {
     let numberInputs = document.querySelectorAll("input[type='number']")
